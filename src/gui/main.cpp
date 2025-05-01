99239137eed0d76d80fcd5d61647989141a4e0ae
aresgornos <aresgornos@Ameril.>
2025-05-01 11:26:07 +0200

    Darkmode

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,11 +1,20 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include <QFile>
+#include <QTextStream>
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    QFile styleFile(":/darkstyle.qss");
+    if (styleFile.open(QFile::ReadOnly | QFile::Text)) {
+        QTextStream ts(&styleFile);
+        QString styleSheet = ts.readAll();
+        w.setStyleSheet(styleSheet);
+    }
+
     w.show();
     return a.exec();
 }
