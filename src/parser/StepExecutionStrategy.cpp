44a1442aea8e9f58e0cf6b142158375cec9ff3c1
Miroslav <mirkobasista@gmail.com>
2025-05-08 20:23:56 +0200

    add step simulation

--- src/parser/StepExecutionStrategy.cpp
+++ src/parser/StepExecutionStrategy.cpp
@@ -1,6 +1,122 @@
 #include "StepExecutionStrategy.h"
 #include "MooreMachine.h"
+#include <QDebug>
+
+StepExecutionStrategy::StepExecutionStrategy(ActionExecutor &actionExecutor,
+                                             MooreMachine &mooreMachine,
+                                             QObject *parent)
+    : QObject(parent),
+      IExecutionStrategy(),
+      m_actionExecutor(actionExecutor),
+      m_mooreMachine(mooreMachine)
+{
+    reset();
+}
 
 void StepExecutionStrategy::Execute()
 {
+    reset();
+    initializeVariables();
+    auto inputResult = m_actionExecutor.evaluate("input");
+    terminalLog("Initial input value: " + inputResult.toString(), Info);
+}
+
+void StepExecutionStrategy::initializeVariables()
+{
+    for (const QString &input : m_mooreMachine.getInputs())
+    {
+        m_actionExecutor.evaluate(input);
+    }
+    for (const QString &output : m_mooreMachine.getOutputs())
+    {
+        m_actionExecutor.evaluate(output);
+    }
+    for (const QString &variable : m_mooreMachine.getVariables())
+    {
+        m_actionExecutor.evaluate(variable);
+    }
+    m_actionExecutor.evaluate("var index = 0;");
+}
+
+bool StepExecutionStrategy::step()
+{
+    if (m_finished || !m_currentState)
+    {
+        return false;
+    }
+
+    executeStateOutput();
+
+    if (evaluateTransitions())
+    {
+        return true;
+    }
+
+    finalizeExecution();
+    return false;
+}
+
+void StepExecutionStrategy::executeStateOutput()
+{
+    auto result = m_actionExecutor.evaluate(m_currentState->getOutput());
+    m_output = result.toString() == "undefined" ? "" : result.toString();
+    terminalLog("State: " + m_currentState->getName() + " Output: " + m_output, Info);
+}
+
+bool StepExecutionStrategy::evaluateTransitions()
+{
+    for (const auto &transition : m_currentState->getTransitions())
+    {
+        auto boolResult = m_actionExecutor.evaluate(transition.getInput());
+        terminalLog("Condition result: " + QString(boolResult.toBool() ? "true" : "false"), TransitionResult);
+
+        if (boolResult.toBool())
+        {
+            m_actionExecutor.evaluate("index++;");
+            m_currentState->unsetCurrent();
+            m_currentState = m_mooreMachine.getState(transition.getTarget());
+            m_currentState->setCurrent();
+            return true;
+        }
+    }
+    return false;
+}
+
+void StepExecutionStrategy::finalizeExecution()
+{
+    m_finished = true;
+    terminalLog("Execution finished in state: " + m_currentState->getName(), Info);
+}
+
+void StepExecutionStrategy::reset()
+{
+    m_currentState->unsetCurrent();
+    m_currentState = m_mooreMachine.getState(m_mooreMachine.getStartState());
+    m_currentState->setCurrent();
+    m_finished = false;
+    m_output.clear();
+
+    if (!m_currentState)
+    {
+        terminalLog("Error: Starting state is null!", Error);
+        m_finished = true;
+    }
+}
+
+void StepExecutionStrategy::terminalLog(QString message, MessageType type)
+{
+    QString prefix;
+    switch (type)
+    {
+    case Error:
+        prefix = "[ERROR] ";
+        break;
+    case Info:
+        prefix = "[INFO] ";
+        break;
+    case TransitionResult:
+        prefix = "[TRANSITION] ";
+        break;
+    }
+    qDebug().noquote() << prefix << message;
 }
--- src/parser/StepExecutionStrategy.h
+++ src/parser/StepExecutionStrategy.h
@@ -1,13 +1,38 @@
 #pragma once
 
-#include "MooreMachine.h"
 #include "IExecutionStrategy.h"
+#include "MooreMachine.h"
+#include "ActionExecutor.h"
+#include <QObject>
+#include <memory>
 
-class StepExecutionStrategy : public IExecutionStrategy
+class StepExecutionStrategy : public QObject, public IExecutionStrategy
 {
+    Q_OBJECT
+
+    enum MessageType
+    {
+        Error,
+        Info,
+        TransitionResult,
+    };
+
 public:
+    explicit StepExecutionStrategy(ActionExecutor &actionExecutor,
+                                   MooreMachine &mooreMachine,
+                                   QObject *parent = nullptr);
+
     void Execute() override;
+    bool step();
+    void reset();
+    void terminalLog(QString message, MessageType type);
+
+    bool isFinished() const { return m_finished; }
 
 private:
-    QString output;
+    QString m_output;
+    bool m_finished = false;
+    std::shared_ptr<MooreState> m_currentState;
+    ActionExecutor &m_actionExecutor;
+    MooreMachine &m_mooreMachine;
 };
